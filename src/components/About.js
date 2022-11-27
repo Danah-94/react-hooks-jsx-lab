5dbbf5cd80bc0c9290f62d6a338d6b0ba497485c
Danah <osta.danah@gmail.com>
2022-11-28 00:07:16 +0200

    Completed Assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+             <h2>About Me</h2>
+             <p><b>"The only way to learn a new programming language is by writing programs in it."</b> - <em>Dennis Ritchie</em> </p>
+             <img src={image} alt="I made this"></img> 
+
+         </div>)
 }
 
 export default About;
